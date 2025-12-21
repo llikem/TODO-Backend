41a1a059827bf7ae371dd29ec3823fe15cfa4eed
llikem <vlad190718@gmail.com>
2025-12-22 02:40:29 +0700

    debuged

--- auth_backend.py
+++ auth_backend.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import hashlib
 import json
 import os
 
 app = FastAPI(title="Todo приложение")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class UserRegister(BaseModel):
     email: str
